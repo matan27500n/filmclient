1619054f98cfae31abb469198f74385c03976bb9
matan27500n <matan27500n@gmail.com>
2021-08-10 15:47:15 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFilmComponent } from './film/add-film/add-film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFilmComponent
   ],
   imports: [
     BrowserModule,
